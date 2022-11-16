1c334d411253e1a98c785f68dda36bb3f762124d
eliott norie <eliott.norie@gmail.com>
2022-11-16 11:43:52 +0100

    mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[new create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+arr = [{ name: 'yo', address: 'ici', phone_number: 'lalala', category: 'french' },
+       { name: "c'est", address: 'la', phone_number: 'lalala', category: 'belgian' },
+       { name: 'mes', address: 'la bas', phone_number: 'lalala', category: 'chinese' },
+       { name: 'bons', address: 'par ici', phone_number: 'lalala', category: 'japanese' },
+       { name: 'restaux', address: 'pat la', phone_number: 'lalala', category: 'italian' }]
+
+arr.each do |restau|
+  Restaurant.create(restau)
+end
